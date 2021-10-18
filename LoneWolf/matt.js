8c14b5a6b448be1aa2ff530c12d9d042a2e20aa4
Matthew Cross <matthewcross1017@gmail.com>
2021-10-18 19:36:41 -0400

    Matt CSS

--- LoneWolf/matt.js
+++ LoneWolf/matt.js
@@ -1,54 +1,61 @@
 var selectButton = document.querySelector("#submitInput");
 var profileValue = document.querySelector("#CPInput");
-var profileRow = document.querySelector("#profileinput");
+var profileRow1 = document.querySelector("#profileinput");
+var profileRow2 = document.querySelector("#profileinput");
 var valueButton = document.querySelector("#submitValue");
-var valueInput = document.querySelector('#valueInput');
-var profileSelect= document.querySelector("#profileSelect");
-var criteriaSelect= document.querySelector("#criteriaSelect");
+var valueInput = document.querySelector("#valueInput");
+var profileSelect = document.querySelector("#profileSelect");
+var criteriaSelect = document.querySelector("#criteriaSelect");
 var criteriaRow = document.querySelector("#profileCriteria1");
-var name = document.querySelector('#Name');
-var profileNames= [{
+var name = document.querySelector("#Name");
+var profileNames = [
+  {
     name: "Profile 1",
     criteria: [
-        {
-            name: "Matt",
-            value: 44
-        },
-        {
-            name: "Force",
-            value: 44
-        }
-    ]
-},
-{
+      {
+        workCompeleted: "x",
+        time: "y",
+      },
+    ],
+  },
+  {
     name: "Profile 2",
     criteria: [
-        {
-            name: "Age",
-            value: 11
-        },
-        {
-            name: "Force",
-            value: 22
-        }
-    ]
-}
-]
-function populateRow() {
-    for(var i = 0; i< profileNames.length; i++){
- profileSelect.textContent = profileNames[i].criteria[i].name;
- var populatedRow = document.createElement("p");
-  populatedRow.textContent = profileSelect;
-  profileRow.appendChild(populatedRow);
-    };
-    
-}
-selectButton.addEventListener("click", populateRow);
+      {
+        workCompeleted: "Age",
+        time: "11",
+      },
+    ],
+  },
+];
+function populateProfile() {
 
-function populateCriteria(){
-if (profileSelect !== null){
+    if (cpSelect.value == "profile") {
+        var inputProfile = document.querySelector("#CPInput");
+        var profileName = inputProfile.value;
+        var name = document.querySelector('#Name');
+        name.textContent= profileName;
+        }
+         else {
+        var inputCriteria = document.querySelector("#CPInput");
+        var profileCriteria = inputCriteria.value;
+        var criteria1 = document.querySelector('#criteria1');
+        criteria1.textContent= profileCriteria;}
+//   for (var i = 0; i < profileNames.length; i++) {
+//    var selectProfile = profileSelect.textContent = profileNames[i].name[i];
+//     var populatedRow = document.createElement("p");
+//     var populatedRow2 = document.createElement("p");
+//    console.log(populatedRow.textContent = selectProfile);
+//     profileRow1.appendChild(populatedRow);
+//     populatedRow2.appendChild()}
+}
+selectButton.addEventListener("click", populateProfile);
+
+function populateCriteria() {
+  if (profileSelect !== null) {
     var populateCriteria1 = document.createElement("p");
-    populateCriteria1.textContent= valueInput.value;
-   (criteriaRow.appendChild(populateCriteria1));
-}}
-valueButton.addEventListener("click",populateCriteria);
+    populateCriteria1.textContent = valueInput.value;
+    criteriaRow.appendChild(populateCriteria1);
+  }
+}
+valueButton.addEventListener("click", populateCriteria);
