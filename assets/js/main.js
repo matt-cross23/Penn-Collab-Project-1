7b895d68de1f1e7ea0182eaac30511ec22d14489
Mike Pizzica <mike.pizzica@gmail.com>
2021-10-18 19:55:42 -0400

    add working time date and title to top of page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 // declare date on page load as a variable and print to page
+console.log("i'm connected");
+
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 $("#currentTime").text(today.format("h:mm a"));
